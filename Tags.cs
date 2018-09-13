a2c825f506a0f9a2eb91d6506de30ee46b4448b0
sophia daniels <sophiathekitty@users.noreply.github.com>
2018-09-12 22:37:59 -0600

    basic unity tags and tags as separate class that extends tags.

---
+++ Tags.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags
+{
+    public static string Player = "Player";
+    public static string Respawn = "Respawn";
+    public static string Finish = "Finish";
+    public static string EditorOnly = "EditorOnly";
+    public static string MainCamera = "MainCamera";
+    public static string GameController = "GameController";
+
+}
---
+++ TagsUFPS.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagsUFPS : Tags
+{
+    public static string Triggerer = "Triggerer";
+    public static string Grass = "Grass";
+    public static string Stone = "Stone";
+}
